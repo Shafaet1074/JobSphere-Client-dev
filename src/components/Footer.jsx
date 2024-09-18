d29f88999c436955aae4e08eefe239bf4c5822d6
huntermahmud <mahmudthedatahunter@gmail.com>
2024-09-18 21:44:46 +0600

    add: navbar and footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            {/* footer will appear here */}
+            this is footer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            navbar will appear here
+        </div>
+    );
+};
+
+export default Navbar;

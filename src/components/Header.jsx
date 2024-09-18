646f2f9296a444b07b01d65aa0dab01959d48f05
huntermahmud <mahmudthedatahunter@gmail.com>
2024-09-18 18:07:29 +0600

    initial setup of JobSphere app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            this is heading from another component.
+        </div>
+    );
+};
+
+export default Header;
